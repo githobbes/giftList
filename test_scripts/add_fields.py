91e2da7c45a6b01faa107da4747897eebd98ff0b
githobbes <phillimj@mail.gvsu.edu>
2022-10-17 16:34:21 -0500

    add borb and plan add_field.py script

---
+++ test_scripts/add_fields.py
@@ -0,0 +1,19 @@
+import borb
+from pathlib import Path
+
+pdf_path = (
+    Path.home()
+    / "PycharmProjects"
+    / "giftlist"
+    / "test_scripts"
+    / "pdf_form_example.pdf"
+)
+
+# Use borb to open pdf_form_example.pdf
+
+# Use borb to read all of the form fields in document
+
+# Use borb to swap a form field out for a different form field
+
+# Use borb to write the document to pdf_form_example_edited.pdf
+
